5b4e34d8ba32138760cbdc30d7c1abf5e086bc00
naysu06 <franzdominicc@gmail.com>
2024-09-10 22:42:28 +0800

    bruh

--- prac0.kt
+++ prac0.kt
@@ -1,7 +1,28 @@
-fun printHello() {
-    println("Hello World")
+fun lbs_kg() {
+    print("Weight in Pounds (lbs): ")
+    val lbs = readln().toDouble()
+    val kg = lbs * 0.4535
+    println("Weight converted to Kilograms (kg): $kg")
+    println("=================================")
+}
+
+fun mi_km() {
+    print("Length in Miles (mi): ")
+    val mi = readln().toDouble()
+    val km = mi * 1.609
+    println("Length in Kilometers (km): $km")
+    println("=================================")
+}
+
+fun fah_cel() {
+    print("Temperature in Fahrenheit (°F): ")
+    val f = readln().toDouble()
+    val c = (f - 32) * 5/9
+    println("Temperature in Fahrenheit (°C): $c.round")
 }
 
 fun main() {
-    printHello()
+    lbs_kg()
+    mi_km()
+    fah_cel()
 }
